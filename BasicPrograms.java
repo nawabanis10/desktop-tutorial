4bbb2943856cc772311b59f112a9daafb06b6512
Muhammad Anis Nawab <nawabanis10@gmail.com>
2026-03-06 11:26:37 +0500

    Replace Student and Book classes with BasicPrograms

--- BasicPrograms.java
+++ BasicPrograms.java
@@ -1,42 +1,49 @@
-class Student {
-    int id;
-    String name;
+public class BasicPrograms {
 
-    void displayStudent() {
-        System.out.println("student id: " + id);
-        System.out.println("student name: " + name);
-    }
-
-}
-
-class Book {
-    int id;
-    String name;
-
-    void displayBook() {
-        System.out.println("book id: " + id);
-        System.out.println("book name: " + name);
-    }
-}
-
-public class Library {
     public static void main(String[] args) {
-        // creat student object
-        Student s1 = new Student();
-        s1.id = 73611;
-        s1.name = "anis nawab";
 
-        // creat book object
-        Book s2 = new Book();
-        s2.id = 89765;
-        s2.name = "java";
+        // 1. Find length of array
+        int arr[] = {10, 20, 30, 40, 50};
+        System.out.println("Length of array: " + arr.length);
 
-        // issue book
-        System.out.println("Book issued successfuly");
 
-        // Display details
-        s1.displayStudent();
-        s2.displayBook();
+        // 2. Prime number check
+        int num = 7;
+        boolean prime = true;
 
+        for(int i = 2; i < num; i++) {
+            if(num % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if(prime)
+            System.out.println(num + " is a Prime Number");
+        else
+            System.out.println(num + " is Not a Prime Number");
+
+
+        // 3. Factorial
+        int n = 5;
+        int fact = 1;
+
+        for(int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial of " + n + " is: " + fact);
+
+
+        // 4. Star Pattern
+        System.out.println("Star Pattern:");
+
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
+
